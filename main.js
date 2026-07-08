b35c35ae11aabee008cef68f068b1b33bfb61754
HediyeSeza <sezahedi13@gmail.com>
2026-07-08 10:10:09 +0330

    Implement dark/light mode

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
-import { initSidebar } from "./src/components/sidebar.js";
-import { initHeader } from "./src/components/header.js";
-import { initTasks } from "./src/components/task.js";
-import { initModal } from "./src/components/modal.js";
+// import { initSidebar } from "./src/components/sidebar.js";
+// import { initHeader } from "./src/components/header.js";
+// import { initTasks } from "./src/components/task.js";
+// import { initModal } from "./src/components/modal.js";
 
-import { initStorage } from "./src/utils/storage.js";
-import { initTheme } from "./src/utils/theme.js";
+// import { initStorage } from "./src/utils/storage.js";
 
-function initApp() {
-  initStorage();
-  initTheme();
+// function initApp() {
+//   initStorage();
 
-  initSidebar();
-  initHeader();
-  initTasks();
-  initModal();
+//   initSidebar();
+//   initHeader();
+//   initTasks();
+//   initModal();
+// }
+
+// initApp();
+
+const lightBtn = document.getElementById("light-btn");
+const darkBtn = document.getElementById("dark-btn");
+
+function setTheme(theme) {
+  if (theme === "dark") {
+    document.documentElement.classList.add("dark");
+
+    darkBtn.classList.add("active");
+    lightBtn.classList.remove("active");
+  } else {
+    document.documentElement.classList.remove("dark");
+
+    lightBtn.classList.add("active");
+    darkBtn.classList.remove("active");
+  }
+
+  localStorage.setItem("theme", theme);
 }
 
-initApp();
+lightBtn.addEventListener("click", () => setTheme("light"));
+darkBtn.addEventListener("click", () => setTheme("dark"));
+
+setTheme(localStorage.getItem("theme") || "light");
