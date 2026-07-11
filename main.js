69d0a9c2be69bbd79dc603ce21903fb6cb1b79ec
HediyeSeza <sezahedi13@gmail.com>
2026-07-11 19:07:14 +0330

    feat: improve dark/light sidebar theme

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-// import { initSidebar } from "./src/components/sidebar.js";
-// import { initHeader } from "./src/components/header.js";
-// import { initTasks } from "./src/components/task.js";
-// import { initModal } from "./src/components/modal.js";
-
-// import { initStorage } from "./src/utils/storage.js";
-
-// function initApp() {
-//   initStorage();
-
-//   initSidebar();
-//   initHeader();
-//   initTasks();
-//   initModal();
-// }
-
-// initApp();
-
 const lightBtn = document.getElementById("light-btn");
 const darkBtn = document.getElementById("dark-btn");
 
@@ -39,3 +21,35 @@ lightBtn.addEventListener("click", () => setTheme("light"));
 darkBtn.addEventListener("click", () => setTheme("dark"));
 
 setTheme(localStorage.getItem("theme") || "light");
+
+//تاریخ//
+
+function updateCurrentDate() {
+  const today = new Date();
+
+  const formatter = new Intl.DateTimeFormat("fa-IR-u-ca-persian", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const parts = formatter.formatToParts(today);
+
+  // debug
+
+  const weekday = parts.find((p) => p.type === "weekday")?.value;
+  const day = parts.find((p) => p.type === "day")?.value;
+  const month = parts.find((p) => p.type === "month")?.value;
+  const year = parts.find((p) => p.type === "year")?.value;
+
+  const currentDate = `${weekday}، ${day} ${month} ${year}`;
+
+  const desktopDate = document.getElementById("desktop-date");
+  const mobileDate = document.getElementById("mobile-date");
+
+  if (desktopDate) desktopDate.textContent = currentDate;
+  if (mobileDate) mobileDate.textContent = currentDate;
+}
+
+updateCurrentDate();
